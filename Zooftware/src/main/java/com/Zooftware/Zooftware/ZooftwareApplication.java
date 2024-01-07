f1541324983ef28dd0ecb16839f0dd53884b4e3e
[skatch27] <[jordysmppc@hotmail.com]>
2024-01-07 11:57:52 +0100

    Modificacion estructura carpetas

---
+++ Zooftware/src/main/java/com/Zooftware/Zooftware/ZooftwareApplication.java
@@ -0,0 +1,13 @@
+package com.Zooftware.Zooftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooftwareApplication.class, args);
+	}
+
+}
---
+++ Zooftware/src/test/java/com/Zooftware/Zooftware/ZooftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zooftware.Zooftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
