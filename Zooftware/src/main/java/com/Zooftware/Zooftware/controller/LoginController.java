6b7990eb4a8bec5a9c154a073c25bab0e830dbdf
carlos <carlos.garridoj@edu.uah.es>
2024-01-17 01:03:06 +0100

    estado mal

--- Zooftware/src/main/java/com/Zooftware/Zooftware/controller/LoginController.java
+++ Zooftware/src/main/java/com/Zooftware/Zooftware/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.Zooftware.Zooftware.controller;
 
 import com.Zooftware.Zooftware.exceptions.ValidacionException;
-import com.Zooftware.Zooftware.repository.PersonaRepository;
-import com.Zooftware.Zooftware.service.PersonaService;
+import com.Zooftware.Zooftware.service.persona.PersonaService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
